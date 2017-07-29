b81bd542c06340fa28eda854b5420bd4f3e9def5
Jacek Góraj <jacek.goraj@gft.com>
2017-07-29 16:33:20 +0200

    refactored code

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,38 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'form',
     template: `
-        <div *ngIf="isShown;else withoutData" class="panel panel-info">
-            <div class="panel-heading">Click button to show information about author!</div>
-            <div class="panel-body">
+        <div class="panel panel-info">
+            <div class="panel-heading">Author's information.</div>
+            <div class="panel-body" *ngIf="isShown;else withoutData">
                 <p>Name: {{authorsData.name}} </p>
                 <p> Birth day: {{authorsData.birthDate}} </p>
                 <p> Planet: {{authorsData.location.planet}} </p>
                 <p> Country: {{authorsData.location.country}} </p>
                 <p> City: {{authorsData.location.city}} </p>
+                <ng-template #withoutData>
+                    <p>Name: - </p>
+                    <p> Birth day: - </p>
+                    <p> Planet: - </p>
+                    <p> Country: - </p>
+                    <p> City: - </p>
+                </ng-template>
             </div>
-            <div class="panel-footer"><a class="btn btn-info" (click)="showHideData()">Click it</a></div>
+            <div class="panel-footer"><a [text]="buttonText" class="btn btn-info" (click)="showHideData()"></a></div>
         </div>
-        <ng-template #withoutData>
-            <div class="panel panel-info">
-            <div class="panel-heading">Click button to show information about author!</div>
-            <div class="panel-body">
-                <p>Name: - </p>
-                <p> Birth day: - </p>
-                <p> Planet: - </p>
-                <p> Country: - </p>
-                <p> City: - </p>
-            </div>
-            <div class="panel-footer"><a class="btn btn-info" (click)="showHideData()">Click it</a></div>
-        </div>
-        </ng-template>
-    `
+    `,
+    styleUrls: ['./form.component.css']
 })
 export class FormComponent {
     
     constructor() {}
     private isShown = false;
+    private buttonText = "Show";
     private authorsData = {
         name: 'Jacek Goraj',
         birthDate: '21/03/1993',
@@ -44,5 +40,7 @@ export class FormComponent {
     }
     showHideData() {
         this.isShown = !this.isShown;
+        if (this.isShown) this.buttonText = "Hide";
+            else this.buttonText = "Show";
     }
 }
