01a6bd78c939405119445077e0b3d64b1f5a4ce0
Jacek Góraj <jacek.goraj@gft.com>
2017-08-04 15:01:44 +0200

    added hidden tag

--- src/app/data/api.mockup.ts
+++ src/app/data/api.mockup.ts
@@ -3,13 +3,15 @@ export const mockupData = [
         id: 0,
         name: 'Google',
         description: 'This is a sample link to google.com',
-        url: 'http://www.google.com/'
+        url: 'http://www.google.com/',
+        rate: 9.4
     },
     {
         id: 1,
         name: 'Pluralsight',
         description: 'Great tool for learning stuff',
-        url: 'http://pluralsight.com/'
+        url: 'http://pluralsight.com/',
+        rate: 6.0
     },
     {
         id: 2,
@@ -27,12 +29,14 @@ export const mockupData = [
         id: 4,
         name: 'Reddit',
         description: 'Forum about every topic',
-        url: 'http://www.reddit.com/'
+        url: 'http://www.reddit.com/',
+        rate: 8
     },
     {
         id: 5,
         name: 'Test',
         description: 'Very long description. Very long description. Very long description. Very long description. Very long description. Very long description. Very long description. Very long description. Very long description. Very long description. ',
-        url: 'http://www.nothing.com/'
+        url: 'http://www.nothing.com/',
+        rate: 1.1
     }    
 ]
--- src/app/useful-links/useful-link.component.ts
+++ src/app/useful-links/useful-link.component.ts
@@ -3,20 +3,31 @@ import { Component, Input } from '@angular/core';
 @Component({
     selector: 'useful-link',
     template: `
-        <div class="panel panel-info usefulLink">
-            <div class="panel-heading">{{ link.name }}</div>
-            <div class="panel-body">
-                <p>{{ link.description }}</p>
-                <p>{{ link.url }}</p>
+        <div class="panel usefulLink">
+            <div class="panel panel-info">
+                <div class="panel-heading">{{ link.name }}</div>
+                <div class="panel-body">
+                    <p>{{ link.description }}</p>
+                    <p [hidden]="shouldHideUrl">{{ link.url }}
+                        <a class="btn btn-info" (click)="showHideUrl()">Hide</a>
+                    </p>
+                    <p [hidden]="!shouldHideUrl"> <a class="btn btn-success" (click)="showHideUrl()">Show</a></p>
+                    <p *ngIf="link.rate">Rate: {{ link.rate }}</p>
+                </div>
             </div>
         </div>
     `,
     styles: [`
-        .usefulLink { min-height: 200px }
+        .usefulLink { min-height: 200px; }
     `]
 })
 export class UsefulLinkComponent {
     @Input() private link: any;
-    
+    private shouldHideUrl: boolean = false;
+
     constructor() { }
+
+    showHideUrl() {
+        this.shouldHideUrl = !this.shouldHideUrl;
+    }
 }
