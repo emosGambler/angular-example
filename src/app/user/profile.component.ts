3b780acd88332c7167f562465ce3a0f385b7258b
Jacek Góraj <jckgrj@gmail.com>
2017-08-27 15:32:39 +0200

    fixed edit profile fields validation

--- src/app/user/profile.component.ts
+++ src/app/user/profile.component.ts
@@ -10,9 +10,14 @@ import * as toastr from 'toastr';
         .error input {
             background-color: #cc0000;
         }
+        em {
+            color: red;
+        }
     `]
 })
 export class ProfileComponent implements OnInit{
+    private firstName: FormControl;
+    private lastName: FormControl;
     profileForm: FormGroup;
 
     constructor(private authService: AuthService,
@@ -20,17 +25,19 @@ export class ProfileComponent implements OnInit{
     ) { };
 
     ngOnInit() {
-        let firstName = new FormControl(
-            this.authService.currentUser.firstName,
-            Validators.required
+        this.firstName = new FormControl(
+            this.authService.currentUser.firstName, [
+                Validators.required, 
+                Validators.pattern('[a-zA-Z].*')
+            ]
         );
-        let lastName = new FormControl(
+        this.lastName = new FormControl(
             this.authService.currentUser.lastName,
             Validators.required
         );
         this.profileForm = new FormGroup({
-            firstName: firstName,
-            lastName: lastName
+            firstName: this.firstName,
+            lastName: this.lastName
         })
     }
 
@@ -44,5 +51,13 @@ export class ProfileComponent implements OnInit{
             this.router.navigate(['comments']);
             toastr.success('Successfuly updated user!');
         };
+    }
+
+    isFirstNameValid() {
+        return this.firstName.valid || this.firstName.untouched;
+    }
+
+    isLastNameValid() {
+        return this.lastName.valid || this.lastName.untouched;
     }
 }
