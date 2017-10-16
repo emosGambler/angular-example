3a9bf9c889b55aa2597f50d08bae8e3191dfc25b
Jacek Góraj <jckgrj@gmail.com>
2017-10-16 21:07:17 +0200

    visual change in component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,22 +3,21 @@ import { Component, Inject, Input, OnInit, ViewContainerRef } from '@angular/cor
 import { ToastsManager,  } from 'ng2-toastr/ng2-toastr';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  constructor(
-    private authService: AuthService,
-    private toastr: ToastsManager,
-    private vRef: ViewContainerRef
-  ) { 
-    this.toastr.setRootViewContainerRef(vRef);
-  }
-  
-  title = 'Angular test application';
-  
-  ngOnInit() {
-    this.toastr.success('Successfully launched testing app');
-  }
+	constructor (
+		private authService: AuthService,
+		private toastr: ToastsManager,
+		private vRef: ViewContainerRef ) { 
+	this.toastr.setRootViewContainerRef(vRef);
+}
+
+	title = 'Angular test application';
+
+	ngOnInit() {
+		this.toastr.success('Successfully launched testing app');
+	}
 }
