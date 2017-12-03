586886c1c4221488c24c6c286a51d91aaa7bcd2e
Jacek Góraj <jckgrj@gmail.com>
2017-12-03 20:45:36 +0100

    changed structure of navbar

--- src/app/navbar.component.ts
+++ src/app/navbar.component.ts
@@ -1,9 +1,33 @@
-import { Component } from '@angular/core';
+import { AuthService } from './user/auth.service';
+import { Component, Inject, Input, OnInit, ViewContainerRef } from '@angular/core';
+import { ISession } from './session/session.model';
+import { ToastrService } from 'ngx-toastr';
+import { CommentsService } from './comment/shared/comments.service';
 
 @Component({
     selector: 'navbar',
     templateUrl: './navbar.component.html'
 })
-export class NavbarComponent {
-    private title: string = 'Angular Test App';
+export class NavbarComponent implements OnInit {
+    private foundSessions: ISession[];
+    private searchValue: string = '';
+    private title: string = 'Angular test application';
+
+    constructor (
+        private authService: AuthService,
+        private commentService: CommentsService,
+        private toastr: ToastrService,
+        private vRef: ViewContainerRef ) { }
+
+    ngOnInit() {
+        this.toastr.success('Successfully launched testing app', 'lol!');
+    }
+
+    private searchSession(searchValue: string) {
+        this.commentService.searchSessions(searchValue).subscribe(
+            sessions => {
+                this.foundSessions = sessions;
+            }
+        );
+    }
 }
